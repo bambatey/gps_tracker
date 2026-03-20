cc38257b2f3d04eddc9047378ffc7bc37b8de64f
enesatila1 <enesatila01@gmail.com>
2026-03-20 21:46:39 +0300

    fix

--- src/config.py
+++ src/config.py
@@ -2,10 +2,19 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql+pg8000://postgres:12.34.qw.er.@45.88.137.131:5432/gps"
+    db_user: str = "postgres"
+    db_password: str = "12.34.qw.er."
+    db_name: str = "gps"
+    database_url: str = ""
 
     class Config:
         env_file = ".env"
+        extra = "ignore"
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if not self.database_url:
+            self.database_url = f"postgresql+pg8000://{self.db_user}:{self.db_password}@postgres:5432/{self.db_name}"
 
 
 settings = Settings()
